fc22b3eb4e1706d8e509a0d904d1974b86f959a6
mohsen1299@gmail.com <mohsen1299@gmail.com>
2024-01-11 19:01:54 +0330

    read wallet descriptor from .env file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+
+    dotenv::from_filename(".env").ok();
+    dotenv::dotenv().ok();
+
+    let result_descriptor = std::env::var("WALLET_DESCRIPTOR ");
+
+    let descriptor = match result_descriptor {
+        Ok(descriptor) => descriptor,
+        Err(e) => panic!("Error: {}", e),
+    };
+    
+    print!("Descriptor: {}", descriptor);
+    dbg!(descriptor);
 }
